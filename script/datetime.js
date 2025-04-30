5e49da5b1e522758f5fc54f654668cd828fd001b
mahmudur2015 <sumonmahmud.2015@gmail.com>
2025-05-01 01:56:48 +0600

    card design added

---
+++ script/datetime.js
@@ -0,0 +1,25 @@
+
+function fnDayName() {
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const today = new Date(); 
+    const weekDayName = days[today.getDay()]; 
+    document.getElementById("current-dayName").innerHTML = weekDayName+" ,";
+}
+
+function fnCurrentDate() {
+    const monthsName = ["Jan", "Feb", "Mar", "Apr", "May", "June", "july","Aug","Sep","Oct","Nov","Dec"];
+
+    const currentDate = new Date();
+    const month = monthsName[currentDate.getMonth() + 1];//Month's Name
+    const day = currentDate.getDate();
+    const year = currentDate.getFullYear();
+    const onlyDate = document.getElementById('current-date')
+    onlyDate.innerText = `${month}-${day}-${year}`
+
+}
+
+
+
+
+
+// console.log(`Current Time: ${hours}:${minutes}:${seconds}`);
